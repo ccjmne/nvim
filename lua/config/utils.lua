4fbb97b2a912a89874a3151221ff2725214de11e
Éric NICOLAS <ccjmne@gmail.com>
2024-12-14 00:54:33 +0100

    Make the highlight on yank shorter

--- lua/config/utils.lua
+++ lua/config/utils.lua
@@ -1,4 +1,4 @@
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking text',
-  callback = function() vim.highlight.on_yank() end,
+  callback = function() vim.highlight.on_yank({ timeout = 50 }) end,
 })
