db040443b53ec203f8aa283360e927d2f28b426f
Éric NICOLAS <ccjmne@gmail.com>
2024-12-14 21:40:10 +0100

    Building neovim from the source code now

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,9 +11,5 @@ return {
   },
   config = function()
     require('lspconfig').lua_ls.setup {}
-    -- These will ship by default with 0.11, presumably
-    vim.keymap.set('n', 'grn', vim.lsp.buf.rename)
-    vim.keymap.set('n', 'gra', vim.lsp.buf.code_action)
-    vim.keymap.set('n', 'grr', vim.lsp.buf.references)
   end
 }
