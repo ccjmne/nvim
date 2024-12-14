c81afcf9d44220a9842ba4de76d411a858c5c4e3
Éric NICOLAS <ccjmne@gmail.com>
2024-12-14 01:09:20 +0100

    Set up undotree

---
+++ lua/plugins/undotree.lua
@@ -0,0 +1,19 @@
+return {
+  'mbbill/undotree',
+  event = 'VeryLazy',
+  vim.keymap.set('n', '<leader>tu', '<CMD>UndotreeToggle<CR>', { desc = 'Toggle Undotree' }),
+  config = function()
+    vim.g.undotree_WindowLayout = 1
+    vim.g.undotree_DiffAutoOpen = 0
+    vim.g.undotree_SetFocusWhenToggle = 1
+    vim.g.undotree_HelpLine = 0
+    vim.g.undotree_DiffCommand = 'git diff --no-index'
+    vim.g.undotree_SplitWidth = 30
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = { 'undotree' },
+      callback = function()
+        vim.keymap.set('n', 'gq', '<CMD>UndotreeToggle<CR>', { buffer = 0 })
+      end,
+    })
+  end,
+}
