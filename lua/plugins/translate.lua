bd14c2e7875c21831c562bf813fd33637d774ca6
Éric NICOLAS <ccjmne@gmail.com>
2024-12-14 16:40:38 +0100

    Configure hotkeys to translate to/from en, de, fr

--- lua/plugins/translate.lua
+++ lua/plugins/translate.lua
@@ -1,11 +1,47 @@
 return {
   'uga-rosa/translate.nvim',
   cmd = 'Translate',
-  keys = { { '<Leader>tr', '<CMD>Translate en<CR>', mode = { 'n', 'v' }, desc = 'Translate selection to English' } },
+  keys = function()
+    local keys = {
+      { '<Leader>tr', '<CMD>Translate en<CR>', mode = { 'n', 'v' }, desc = 'Translate selection to English' },
+      { '<Leader>tr.', '<CMD>Translate en<CR>', mode = { 'n', 'v' }, desc = 'Translate selection to English' },
+    }
+    local langs = { e = 'en', d = 'de', f = 'fr' }
+    for l, L in pairs(langs) do
+      table.insert (keys, { '<Leader>tr'..l, '<CMD>Translate '..L..'<CR>', mode = { 'n', 'v' }, desc = 'Translate selection to '..string.upper(L) })
+      table.insert (keys, { '<Leader>tr'..l..'.', '<CMD>Translate '..L..'<CR>', mode = { 'n', 'v' }, desc = 'Translate selection to '..string.upper(L) })
+      for r, R in pairs(langs) do
+        if r ~= l then
+          table.insert (keys, {
+            '<Leader>tr'..l..r,
+            '<CMD>Translate '..L..' -source='..R..'<CR>',
+            mode = { 'n', 'v' },
+            desc = 'Translate selection to '..string.upper(L)..' from '..string.upper(R)
+          })
+        end
+      end
+    end
+    return keys
+  end,
   config = {
     default = {
       command = 'google',
       output = 'replace',
+    },
+    silent = true,
+    preset = {
+      parse_before = {
+        natural = {
+          lang_abbr = {
+            en = 'english',
+            de = 'english',
+            fr = 'english',
+          },
+          end_marks = {
+            english = { '.', '?', '!', ':', ';' },
+          },
+        },
+      },
     },
   },
 }
