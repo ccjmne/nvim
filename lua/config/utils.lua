11bd5eb74a0ca25aa54c88607271a4b251eaeff7
Éric NICOLAS <ccjmne@gmail.com>
2024-12-15 23:30:58 +0100

    Borrow n-Esc and n-Leaderq from Kickstart

--- lua/config/utils.lua
+++ lua/config/utils.lua
@@ -6,3 +6,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 vim.keymap.set('n', '<Leader>x', '<CMD>.lua<CR>')
 vim.keymap.set('n', '<Leader><Leader>x', '<CMD>source %<CR>')
 vim.keymap.set('v', '<Leader>x', ':lua<CR>')
+
+vim.keymap.set('n', '<Esc>', '<CMD>nohlsearch<CR>')
+vim.keymap.set('n', '<Leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
