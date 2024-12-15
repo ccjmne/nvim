8bbdba03bc26099cb42f997d8a4962cc3ca8a5f1
Éric NICOLAS <ccjmne@gmail.com>
2024-12-15 22:05:20 +0100

    Set up lsp_lines

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,15 +1,37 @@
 return {
-  'neovim/nvim-lspconfig',
-  dependencies = {
-    'folke/lazydev.nvim',
-    ft = 'lua',
-    opts = {
-      library = {
-        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+  {
+    'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
+    config = function()
+      require('lsp_lines').setup()
+    end,
+    keys = function(_, keys)
+      local inline = true
+      return {
+        {
+          '<leader>tq',
+          function()
+            inline = not inline
+            vim.diagnostic.config { virtual_text = inline, virtual_lines = not inline }
+          end,
+          desc = '[T]oggle Diagnostics Style',
+        },
+        unpack(keys),
+      }
+    end,
+  },
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      'folke/lazydev.nvim',
+      ft = 'lua',
+      opts = {
+        library = {
+          { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+        },
       },
     },
-  },
-  config = function()
-    require('lspconfig').lua_ls.setup {}
-  end
+    config = function()
+      require('lspconfig').lua_ls.setup {}
+    end
+  }
 }
