0ddd9b6c127d9bfb6548b6991e21f14d55f85ee7
Éric NICOLAS <ccjmne@gmail.com>
2024-12-21 21:36:54 +0100

    Introduce <Leader>fn,fm to find nvim and dotfiles

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,6 +1,8 @@
 return {
   'ibhagwan/fzf-lua',
   keys = {
-    { '<Leader>ff', function() require 'fzf-lua'.files {} end, desc = '[F]ind [F]iles' },
+    { '<Leader>ff', function() require 'fzf-lua'.files {} end,                                        desc = '[F]ind [F]iles' },
+    { '<Leader>fn', function() require 'fzf-lua'.files { cwd = vim.fn.stdpath 'config' } end,         desc = '[F]ind in [N]eovim Config' },
+    { '<Leader>fm', function() require 'fzf-lua'.files { cwd = vim.env.HOME .. '/git/dotfiles' } end, desc = '[F]ind in [M]y Dotfiles' },
   },
 }
