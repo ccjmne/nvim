b7b72180b5aa2af2f9b00baa886fb725ffd38787
Éric NICOLAS <ccjmne@gmail.com>
2024-12-21 21:35:55 +0100

    Switch to <Leader>f prefix for fzf-lua
    For "find" rather than "search", more aligned with that "fzf" stands for
    in the first place.

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,6 +1,6 @@
 return {
   'ibhagwan/fzf-lua',
   keys = {
-    { '<Leader>sf', function() require('fzf-lua').files() end, desc = 'Search Files in CWD' },
+    { '<Leader>ff', function() require 'fzf-lua'.files {} end, desc = '[F]ind [F]iles' },
   },
 }
