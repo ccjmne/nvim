e2ac1afc814a28c328be3628db0b789e1a08861f
Éric NICOLAS <ccjmne@gmail.com>
2024-12-22 17:21:40 +0100

    Adopt mason, mason-lspconfig

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -19,16 +19,22 @@ return {
   {
     'neovim/nvim-lspconfig',
     dependencies = {
-      'folke/lazydev.nvim',
-      ft = 'lua',
-      opts = {
-        library = {
-          { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+      {
+        'folke/lazydev.nvim',
+        ft = 'lua',
+        opts = {
+          library = {
+            { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+          },
         },
       },
     },
     config = function()
-      require('lspconfig').lua_ls.setup {}
+      require 'mason'.setup {}
+      require 'mason-lspconfig'.setup()
+      require 'lspconfig'.lua_ls.setup {}
     end
-  }
+  },
 }
