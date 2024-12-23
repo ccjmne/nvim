9059a1688a80f2b9b79060712012288e8f109a61
Éric NICOLAS <ccjmne@gmail.com>
2024-12-23 23:08:22 +0100

    Set up TS and Rust LSPs

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -35,6 +35,8 @@ return {
       require 'mason'.setup {}
       require 'mason-lspconfig'.setup()
       require 'lspconfig'.lua_ls.setup {}
+      require 'lspconfig'.ts_ls.setup {}
+      require 'lspconfig'.rust_analyzer.setup {}
     end
   },
 }
