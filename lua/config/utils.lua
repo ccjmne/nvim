11aad2fe725abde9d366fe9e7849ff23a57906f1
Éric NICOLAS <ccjmne@gmail.com>
2025-01-08 21:26:52 +0100

    Introduce bang to Title to center it

--- lua/config/utils.lua
+++ lua/config/utils.lua
@@ -18,6 +18,7 @@ vim.keymap.set('n', '<C-p>', '<CMD>cprev<CR>', { desc = 'Jump to previous quickf
 
 vim.api.nvim_create_user_command('Title', function(opts)
   local padchar = opts.fargs[1] or '='
+  local center = opts.bang -- Use `Title!` to center the title
   local lo, hi
   if opts.count == -1 then     -- Normal mode, see https://www.petergundel.de/neovim/lua/hack/2023/12/17/get-neovim-mode-when-executing-a-command.html
     lo, hi = vim.fn.line '.', vim.fn.line '.'
@@ -27,13 +28,20 @@ vim.api.nvim_create_user_command('Title', function(opts)
   end
 
   for i = lo, hi do
-    local head = vim.api.nvim_buf_get_lines(0, i - 1, i, false)[1]:gsub('%s*[%.=-]*$', '')
-    local pad = (vim.bo.textwidth > 0 and vim.bo.textwidth or 80) - #head - 1     -- vim."bo" stands for Buffer Options
-    if pad > 0 then
-      vim.api.nvim_buf_set_lines(0, i - 1, i, false, { head .. ' ' .. string.rep(padchar, pad) })
+    local indent = vim.api.nvim_buf_get_lines(0, i - 1, i, false)[1]:match('^%s*')
+    local head = vim.api.nvim_buf_get_lines(0, i - 1, i, false)[1]:gsub('^[%.=-]*%s*', ''):gsub('%s*[%.=-]*$', '')
+    local pad = (vim.bo.textwidth > 0 and vim.bo.textwidth or 80) - #indent - #head - 2
+    if center then
+      if pad > 0 then
+        vim.api.nvim_buf_set_lines(0, i - 1, i, false, { indent .. string.rep(padchar, math.floor(pad / 2)) .. ' ' .. head .. ' ' .. string.rep(padchar, math.ceil(pad / 2)) })
+      end
+    else
+      if pad >=0 then
+        vim.api.nvim_buf_set_lines(0, i - 1, i, false, { indent .. head .. ' ' .. string.rep(padchar, pad + 1) })
+      end
     end
   end
-end, { range = true, nargs = '?' })
+end, { range = true, nargs = '?', bang = true })
 
 vim.api.nvim_create_autocmd('FileType', {
   pattern = { 'help' },
