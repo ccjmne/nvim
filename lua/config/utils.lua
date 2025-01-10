451478e86a97b62f76f1f2d2bed990629702bcb7
Éric NICOLAS <ccjmne@gmail.com>
2025-01-10 15:35:10 +0100

    Fix description for <Leader>l

--- lua/config/utils.lua
+++ lua/config/utils.lua
@@ -11,7 +11,7 @@ vim.keymap.set('n', '<Esc>', '<CMD>nohlsearch<CR>')
 -- Figure out better. See https://github.com/lukas-reineke/dotfiles/blob/master/vim/lua/lists.lua
 -- Also consider https://github.com/kevinhwang91/nvim-bqf
 vim.keymap.set('n', '<Leader>c', vim.diagnostic.setqflist, { desc = 'Set quickfix list' })
-vim.keymap.set('n', '<Leader>l', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<Leader>l', vim.diagnostic.setloclist, { desc = 'Set location list' })
 
 vim.keymap.set('n', '<C-n>', '<CMD>cnext<CR>', { desc = 'Jump to next quickfix entry' })
 vim.keymap.set('n', '<C-p>', '<CMD>cprev<CR>', { desc = 'Jump to previous quickfix entry' })
