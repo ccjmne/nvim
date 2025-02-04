e1553e666b11d060debc6ca649157baaaa659d74
Éric NICOLAS <ccjmne@gmail.com>
2025-02-04 21:41:38 +0100

    Set shortcuts to interact w/ CopilotChat

--- lua/plugins/llm.lua
+++ lua/plugins/llm.lua
@@ -18,13 +18,24 @@ return {
       { 'github/copilot.vim' },                       -- or zbirenbaum/copilot.lua
       { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
     },
-    build = 'make tiktoken',                          -- Only on MacOS or Linux
+    keys = {
+      {
+        '<Leader>cp',
+        function()
+          require('CopilotChat.integrations.fzflua').pick(require('CopilotChat.actions').prompt_actions())
+        end,
+        mode = { 'n', 'v' },
+        desc = 'CopilotChat - Prompt actions',
+      },
+      { '<Leader>ct', '<CMD>CopilotChatToggle<CR>', mode = { 'n', 'v' }, { desc = 'Toggle Copilot Chat' } },
+    },
+    build = 'make tiktoken',
     config = function()
       require 'CopilotChat'.setup {}
       vim.api.nvim_create_autocmd('BufEnter', {
         pattern = { 'copilot-chat' },
         callback = function() vim.wo.wrap = true end,
       })
-    end
+    end,
   },
 }
