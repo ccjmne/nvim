53f264797c437c9e24f1bbc86a54e07752afe71a
Éric NICOLAS <ccjmne@gmail.com>
2025-05-11 15:08:41 +0200

    Upgrade away from deprecated vim.highlight API

--- lua/config/utils.lua
+++ lua/config/utils.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking text',
-  callback = function() vim.highlight.on_yank({ timeout = 50 }) end,
+  callback = function() vim.hl.on_yank({ timeout = 50 }) end,
 })
 
 vim.keymap.set('n', '<Leader>x', '<CMD>.lua<CR>')
