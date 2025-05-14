dbeca7c3ff4ec174b15cb3d283179ef533e71102
Éric NICOLAS <ccjmne@gmail.com>
2025-05-14 21:03:57 +0200

    Switch to <leader>fm for formatting
    One more character, but shall prevent hanging for half a second while
    waiting for possible extra fzf-lua related input, which also start w/
    <leader>f

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -4,7 +4,7 @@ return {
   cmd = { 'ConformInfo' },
   keys = {
     {
-      '<leader>f',
+      '<leader>fm',
       function() require 'conform'.format { async = true, lsp_format = 'fallback' } end,
       mode = '',
       desc = '[F]ormat buffer',
