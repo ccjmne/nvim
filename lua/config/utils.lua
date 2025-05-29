a4529a37af6011621fe2b46a4105291646b85f13
Éric NICOLAS <ccjmne@gmail.com>
2025-05-29 14:45:41 +0200

    Automate folds through LSP w/ treesitter fallback

--- lua/config/utils.lua
+++ lua/config/utils.lua
@@ -1,3 +1,20 @@
+vim.opt.foldlevelstart = 99
+vim.opt.foldnestmax = 4
+
+vim.opt.fillchars:append({ fold = ' ' })
+vim.opt.foldtext = ''
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client:supports_method('textDocument/foldingRange') then
+      local win = vim.api.nvim_get_current_win()
+      vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
+    end
+  end,
+})
+
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking text',
   callback = function() vim.hl.on_yank({ timeout = 50 }) end,
