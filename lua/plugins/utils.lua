bd1f90ed35ae698c56e72adcffbdd92fd50bc830
Éric NICOLAS <ccjmne@gmail.com>
2025-07-27 18:19:14 +0200

    append $GIT_DIR/.vimrt to runtimepath

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -3,4 +3,15 @@ return {
     'stefandtw/quickfix-reflector.vim',
     config = function() end,
   },
+  {
+    dir = '.',
+    config = function()
+      local gitdir = vim.fn.fnamemodify(vim.fn.finddir('.git', '.;') or '', ':p:h')
+      local localrt = vim.fn.resolve(gitdir .. '/../.vimrt')
+      if vim.fn.isdirectory(localrt) == 1 then
+        vim.opt.rtp:append(localrt)
+        vim.notify('Added ' .. localrt .. ' to runtimepath', vim.log.levels.INFO)
+      end
+    end,
+  }
 }
