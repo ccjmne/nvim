280dcc9665e25cd387ad3ffc0514ada738266b3b
Éric NICOLAS <ccjmne@gmail.com>
2025-08-13 16:19:32 +0200

    Prefer more simplistic vim.o to vim.opt

--- init.lua
+++ init.lua
@@ -1,37 +1,37 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.opt.spelllang = 'en_gb'
+vim.o.spelllang = 'en_gb'
 
-vim.opt.number = true
-vim.opt.relativenumber = false
-vim.opt.signcolumn = 'yes'
+vim.o.number = true
+vim.o.relativenumber = false
+vim.o.signcolumn = 'yes'
 
-vim.opt.cursorline = true
-vim.opt.guicursor = ''
-vim.opt.scrolloff = 8
-vim.opt.sidescrolloff = 8
-vim.opt.wrap = false
+vim.o.cursorline = true
+vim.o.guicursor = ''
+vim.o.scrolloff = 8
+vim.o.sidescrolloff = 8
+vim.o.wrap = false
 
-vim.opt.undofile = true
-vim.opt.updatetime = 160
-vim.opt.timeoutlen = 320
-vim.opt.grepprg = vim.fn.executable('rg') == 1 and 'rg --vimgrep' or vim.opt.grepprg
+vim.o.undofile = true
+vim.o.updatetime = 160
+vim.o.timeoutlen = 320
+vim.o.grepprg = vim.fn.executable('rg') == 1 and 'rg --vimgrep' or vim.o.grepprg
 
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.incsearch = true
-vim.opt.inccommand = 'split'
-vim.opt.hlsearch = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.incsearch = true
+vim.o.inccommand = 'split'
+vim.o.hlsearch = true
 
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+vim.o.splitright = true
+vim.o.splitbelow = true
 
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣', precedes = '<', extends = '>' }
+vim.o.list = true
+vim.o.listchars='tab:» ,trail:·,nbsp:␣,precedes:<,extends:>'
 
-vim.opt.laststatus = 1
-vim.opt.foldcolumn = '0'
+vim.o.laststatus = 1
+vim.o.foldcolumn = '0'
 
 require('config.utils')
 require('config.lazy')
