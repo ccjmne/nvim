a7f74757397d2bc4a1c7259ae9c5c6f7143a5840
Éric NICOLAS <ccjmne@gmail.com>
2025-08-13 13:45:39 +0200

    Find local .vimrt in any ancestor to CWD

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -6,9 +6,8 @@ return {
   {
     dir = '.',
     config = function()
-      local gitdir = vim.fn.fnamemodify(vim.fn.finddir('.git', '.;') or '', ':p:h')
-      local localrt = vim.fn.resolve(gitdir .. '/../.vimrt')
-      if vim.fn.isdirectory(localrt) == 1 then
+      local localrt = vim.fn.finddir('.vimrt', ';')
+      if localrt ~= '' then
         vim.opt.rtp:append(localrt)
         vim.notify('Added ' .. localrt .. ' to runtimepath', vim.log.levels.INFO)
       end
