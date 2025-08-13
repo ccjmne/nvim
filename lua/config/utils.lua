77e2fec5558867ba7d8a15dc5dda085b97680ade
Éric NICOLAS <ccjmne@gmail.com>
2025-08-13 20:19:32 +0200

    Polish terminal-in-nvim set-up
    - Turns out that the nonu, nornu is there by default now.
    - Prefer more terse vimscript for terminal opening.
    - Leverage :vertical with :terminal.

--- lua/config/utils.lua
+++ lua/config/utils.lua
@@ -109,18 +109,11 @@ vim.api.nvim_create_autocmd('FileType', {
   end,
 })
 
-vim.api.nvim_create_autocmd('TermOpen', {
-  group = vim.api.nvim_create_augroup('term-open', { clear = true }),
-  callback = function()
-    vim.opt.number = false
-    vim.opt.relativenumber = false
-  end,
-})
-
 vim.keymap.set({ 'n', 't' }, '<Leader>tt', function()
-  vim.cmd('new')
-  vim.cmd('wincmd J')
-  vim.cmd('term')
-  vim.cmd('startinsert')
-  vim.api.nvim_win_set_height(0, 15)
+  vim.cmd [[
+    vertical term
+    wincmd J
+    resize 15
+    startinsert
+  ]]
 end)
