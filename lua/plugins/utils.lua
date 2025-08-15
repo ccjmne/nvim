872031166b8a841e1c3798dc1655a7d42fa4e9d7
Éric NICOLAS <ccjmne@gmail.com>
2025-08-15 17:14:55 +0200

    Bring back .vimrt scanning to some lazy package
    That seems to be the only way to achieve this while delegating plug-in
    management to lazy.nvim.  I really do not like that aspect of it.

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -6,4 +6,17 @@ return {
       vim.cmd('highlight link klogRecord Title')
     end,
   },
+  {
+    -- lazy.nvim will overrwrite runtimepath: this is the only way to do
+    -- that while I'm sticking with lazy.nvim.
+    -- TODO: consider dropping lazy.nvim, move this to init.lua.
+    dir = '.',
+    config = function()
+      local localrt = vim.fn.finddir('.vimrt', ';')
+      if localrt ~= '' then
+        vim.opt.rtp:append(localrt)
+        vim.notify('Added ' .. localrt .. ' to runtimepath', vim.log.levels.INFO)
+      end
+    end,
+  }
 }
