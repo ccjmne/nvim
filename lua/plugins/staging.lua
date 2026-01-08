3e29e0e29c255283e92d933365219690b2fba00b
Éric NICOLAS <ccjmne@gmail.com>
2026-01-08 16:50:46 +0100

    Map ga in visual and normal mode to EasyAlign

--- lua/plugins/staging.lua
+++ lua/plugins/staging.lua
@@ -1,4 +1,13 @@
 return {
+  {
+    'junegunn/vim-easy-align',
+    config = function()
+      vim.cmd [[
+        nmap ga      <Plug>(EasyAlign)
+        vmap <Enter> <Plug>(EasyAlign)
+      ]]
+    end
+  },
   { 'pteroctopus/faster.nvim' },
   { 'MagicDuck/grug-far.nvim' },
 }
